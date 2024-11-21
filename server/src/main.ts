bb0634d1cb675825e4a7cef65043e4937fafd4b0
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2024-11-21 16:35:43 +0100

    feat: remove import from logic on the server for artist

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,5 @@
 // Load environment variables from .env file
 import "dotenv/config";
-import app from "./app";
 
 // Check database connection
 // Note: This is optional and can be removed if the database connection
@@ -8,17 +7,21 @@ import app from "./app";
 import "../database/checkConnection";
 
 // Import the Express application from ./app
+import app from "./app";
+
+const dataArtist = require("../database/data/artist.json");
+
 app.get("/artist", async (req, res) => {
-  const data = require("../database/data/artist.json");
-  res.json(data);
+  res.json(dataArtist);
 });
 app.get("/albums", async (req, res) => {
   const data = require("../database/data/album.json");
   res.json(data);
 });
 app.get("/artist/:id", async (req, res) => {
-  const data = require("../database/data/artist.json");
-  res.json(data.filter((i: { id: number }) => i.id === Number(req.params.id)));
+  res.json(
+    dataArtist.filter((i: { id: number }) => i.id === Number(req.params.id)),
+  );
 });
 app.get("/albums/:id", async (req, res) => {
   const data = require("../database/data/tracklist.json");
